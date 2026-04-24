b8689bc013d169308a8642df54aeb8fa6f600a09
fadykhayratGa <163141137+fadykahyratGa@users.noreply.github.com>
2026-04-24 22:58:42 +0300

    Revert "Remove react-native-config; bump Android version"
    This reverts commit abb1a46f197f8cd6b0e732a61a8f7697f224cc79.

---
+++ svg.d.ts
@@ -0,0 +1,15 @@
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module 'react-native-config' {
+  const Config: Record<string, string | undefined> & {
+    SENTRY_DSN?: string;
+    SENTRY_ENVIRONMENT?: string;
+  };
+  export default Config;
+}
